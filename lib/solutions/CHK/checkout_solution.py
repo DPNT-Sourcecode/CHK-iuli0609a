fa8226a288c7c7a6067c317e903cc703e135d60c
sbx_user1051 <sbx_user1051@169.254.93.53>
2024-09-06 08:20:39 +0000

    Fri Sep 06 08:20:39 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,8 +4,9 @@ from collections import defaultdict
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str) -> int:
-    prices = {"A": 50, "B": 30, "C": 20, "D": 15}
-    offers = {"A": (3, 130), "B": (2, 45)}
+    prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40}
+    offers = {"A": [(3, 130), (5, 200)], "B": [(2, 45)]}
+    extra_item_offers = {"E": [(2, "B")]}
     item_counts = defaultdict(int)
 
     for sku in skus:
@@ -14,13 +15,32 @@ def checkout(skus: str) -> int:
         else:
             item_counts[sku] += 1
 
+    for item, count in item_counts.items():
+        item_extra_item_offers = extra_item_offers.get(item)
+        item_offers = offers.get(item)
+
+        # calculate the free items to be removed from the total after the financial offers
+        if item_extra_item_offers:
+            # apply the best offer first and then work down
+            sorted_special_offers = sorted(item_extra_item_offers, key=lambda x: -x[0])
+            for offer_count, offer_item in sorted_special_offers:
+                free_item_count = count // offer_count
+                number_of_free_item_to_pay_for = item_counts[offer_item] - free_item_count if item_counts[offer_item] > free_item_count else 0
+                item_counts[offer_item] = number_of_free_item_to_pay_for
+                count %= offer_count
+
     total = 0
-    for item, counts in item_counts.items():
-        offer = offers.get(item)
-        if offer:
-            offer_count, offer_price = offer
-            total += (counts // offer_count) * offer_price
-            counts %= offer_count
-        total += counts * prices[item]
+    for item, count in item_counts.items():
+        item_extra_item_offers = extra_item_offers.get(item)
+        item_offers = offers.get(item)
+
+        if item_offers:
+            # apply the best offer first and then work down
+            sorted_offers = sorted(item_offers, key=lambda x: -x[0])
+            for offer_count, offer_price in sorted_offers:
+                total += (count // offer_count) * offer_price
+                count %= offer_count
+        total += count * prices[item]
 
     return total
+
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -14,3 +14,20 @@ class TestCheckout():
         assert checkout_solution.checkout("aaa") == -1
         # test case with none alpha characters
         assert checkout_solution.checkout("AAABBBC123@") == -1
+        # test case with extra item offers
+        assert checkout_solution.checkout("EEB") == 80
+        # test case with extra item offers and financial offers
+        # should get one B free after the 2 for 45 B offer
+        assert checkout_solution.checkout("EEBB") == 110
+        assert checkout_solution.checkout("EEEEBB") == 160
+        # test case with multiple financial offers for one sku
+        assert checkout_solution.checkout("AAAAAA") == 250
+        assert checkout_solution.checkout("AAAAAAAA") == 330
+        assert checkout_solution.checkout("AAAAAAAAA") == 380
+        # test case with mixed SKUs and offers
+        assert checkout_solution.checkout("AABBCCDDEE") == 280
+        assert checkout_solution.checkout("ABCDEABCDE") == 280
+        assert checkout_solution.checkout("CCADDEEBBA") == 280
+        assert checkout_solution.checkout("AAAAAEEBAAABB") == 455
+        assert checkout_solution.checkout("ABCDECBAABCABBAAAEEAA") == 665
+
