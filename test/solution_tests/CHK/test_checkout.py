8c42de6c36cde9383c32bf648f0680c87ec71894
sbx_user1051 <sbx_user1051@169.254.93.53>
2024-09-06 08:29:15 +0000

    Fri Sep 06 08:29:15 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -6,31 +6,16 @@ class TestCheckout():
         # test case with an invald SKU
         assert checkout_solution.checkout("") == 0
         # test case with no items
-        assert checkout_solution.checkout("") == 0
+        assert checkout_solution.checkout("A") == 50
         # test case with no special offers
-        assert checkout_solution.checkout("ABCD") == 115
+        assert checkout_solution.checkout("B") == 30
         # test case with special offers
-        assert checkout_solution.checkout("AAABBBC") == 225
+        assert checkout_solution.checkout("C") == 20
         # test case with lower case SKUs
-        assert checkout_solution.checkout("aaa") == -1
+        assert checkout_solution.checkout("D") == 15
         # test case with none alpha characters
-        assert checkout_solution.checkout("AAABBBC123@") == -1
-        # test case with extra item offers
-        assert checkout_solution.checkout("EEB") == 80
-        # test case with extra item offers and financial offers
-        # should get one B free after the 2 for 45 B offer
-        assert checkout_solution.checkout("EEBB") == 110
-        assert checkout_solution.checkout("EEEEBB") == 160
-        # test case with multiple financial offers for one sku
-        assert checkout_solution.checkout("AAAAAA") == 250
-        assert checkout_solution.checkout("AAAAAAAA") == 330
-        assert checkout_solution.checkout("AAAAAAAAA") == 380
-        # test case with mixed SKUs and offers
-        assert checkout_solution.checkout("AABBCCDDEE") == 280
-        assert checkout_solution.checkout("ABCDEABCDE") == 280
-        assert checkout_solution.checkout("CCADDEEBBA") == 280
-        assert checkout_solution.checkout("AAAAAEEBAAABB") == 455
-        assert checkout_solution.checkout("ABCDECBAABCABBAAAEEAA") == 665
+        assert checkout_solution.checkout("E") == -1
+
 
     def test_checkout(self):
         # test case with an invald SKU
@@ -61,5 +46,6 @@ class TestCheckout():
         assert checkout_solution.checkout("CCADDEEBBA") == 280
         assert checkout_solution.checkout("AAAAAEEBAAABB") == 455
         assert checkout_solution.checkout("ABCDECBAABCABBAAAEEAA") == 665
+
 
 
