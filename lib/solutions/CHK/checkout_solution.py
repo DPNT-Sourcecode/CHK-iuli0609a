8ec1b08bb912d33b9b933605790ffa8bd002fbdc
sbx_user1051 <sbx_user1051@169.254.93.53>
2024-09-06 08:17:27 +0000

    Fri Sep 06 08:17:27 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,26 @@
+from collections import defaultdict
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+def checkout(skus: str) -> int:
+    prices = {"A": 50, "B": 30, "C": 20, "D": 15}
+    offers = {"A": (3, 130), "B": (2, 45)}
+    item_counts = defaultdict(int)
+
+    for sku in skus:
+        if sku not in prices:
+            return -1
+        else:
+            item_counts[sku] += 1
+
+    total = 0
+    for item, counts in item_counts.items():
+        offer = offers.get(item)
+        if offer:
+            offer_count, offer_price = offer
+            total += (counts // offer_count) * offer_price
+            counts %= offer_count
+        total += counts * prices[item]
+
+    return total
---
+++ test/solution_tests/CHK/test_checkout.py
@@ -0,0 +1,16 @@
+from solutions.CHK import checkout_solution
+
+class TestCheckout():
+    def test_checkout(self):
+        # test case with an invald SKU
+        assert checkout_solution.checkout("aabzc") == -1
+        # test case with no items
+        assert checkout_solution.checkout("") == 0
+        # test case with no special offers
+        assert checkout_solution.checkout("ABCD") == 115
+        # test case with special offers
+        assert checkout_solution.checkout("AAABBBC") == 225
+        # test case with lower case SKUs
+        assert checkout_solution.checkout("aaa") == -1
+        # test case with none alpha characters
+        assert checkout_solution.checkout("AAABBBC123@") == -1
